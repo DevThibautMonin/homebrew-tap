68149f812067a36f55857f974bc89b671c282f64
Thibaut Monin <device.thibaut.monin@gmail.com>
2025-12-28 21:47:11 +0100

    initial commit

---
+++ Casks/opengit.rb
@@ -0,0 +1,21 @@
+cask "opengit" do
+  version "1.0.1"
+  sha256 "DUMMY"
+
+  url "https://github.com/thibautmonin/opengit/releases/download/v#{version}/OpenGit-#{version}.dmg"
+  name "OpenGit"
+  desc "OpenGit is a modern desktop Git client designed to provide a clear, visual Git experience, while remaining powerful enough for everyday development workflows."
+  homepage "https://github.com/DevThibautMonin/open_git"
+
+  depends_on macos: ">= :catalina"
+
+  app "OpenGit.app"
+
+  uninstall quit: "com.thibautmonin.opengit"
+
+  zap trash: [
+    "~/Library/Application Support/OpenGit",
+    "~/Library/Preferences/com.thibautmonin.opengit.plist",
+    "~/Library/Caches/com.thibautmonin.opengit"
+  ]
+end
